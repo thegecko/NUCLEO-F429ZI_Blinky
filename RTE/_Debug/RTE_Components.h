a7de6be54e680b1a3cf1589832142b3f72b6fa78
thegecko <rob.moran@arm.com>
2021-09-08 18:57:25 +0100

    Initial Commit

---
+++ RTE/_Debug/RTE_Components.h
@@ -0,0 +1,60 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.3 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube HAL:Common:1.7.9 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.7.9 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA:1.7.9 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:ETH:1.7.9 */
+#define RTE_DEVICE_HAL_ETH
+/* Keil::Device:STM32Cube HAL:GPIO:1.7.9 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:PCD:1.7.9 */
+#define RTE_DEVICE_HAL_PCD
+/* Keil::Device:STM32Cube HAL:PWR:1.7.9 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.7.9 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:UART:1.7.9 */
+#define RTE_DEVICE_HAL_UART
+/* Keil::Device:STM32Cube LL:Common:1.7.9 */
+#define RTE_DEVICE_LL_COMMON
+/* Keil::Device:STM32Cube LL:GPIO:1.7.9 */
+#define RTE_DEVICE_LL_GPIO
+/* Keil::Device:STM32Cube LL:PWR:1.7.9 */
+#define RTE_DEVICE_LL_PWR
+/* Keil::Device:STM32Cube LL:RCC:1.7.9 */
+#define RTE_DEVICE_LL_RCC
+/* Keil::Device:STM32Cube LL:UTILS:1.7.9 */
+#define RTE_DEVICE_LL_UTILS
+/* Keil::Device:Startup:2.6.3 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+
+
+#endif /* RTE_COMPONENTS_H */
